0796fa20162fb114226a41f057bee9d4fb019a96
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-01-06 21:06:00 +0530

    Added better default message and thumbnail
    Co-Authored-by: Tanveer Singh <79406661+tanxveer@users.noreply.github.com>
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/src/main/java/com/gdsc/bbsbec/gbooks/model/ImageLinks.kt
+++ app/src/main/java/com/gdsc/bbsbec/gbooks/model/ImageLinks.kt
@@ -21,7 +21,7 @@ import com.google.gson.annotations.SerializedName
 
 data class ImageLinks(
 
-    @SerializedName("smallThumbnail") var smallThumbnail: String? = null,
-    @SerializedName("thumbnail") var thumbnail: String? = null
+    @SerializedName("smallThumbnail") var smallThumbnail: String? = "https://i.imgur.com/YjoNXCX.png",
+    @SerializedName("thumbnail") var thumbnail: String? = "https://i.imgur.com/YjoNXCX.png"
 
 )
